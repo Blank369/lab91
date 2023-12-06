b55fa61f04478a4d3a2bae89543bf53744a77449
Blank36 <nikolaenkoyu36@gamil.com>
2023-12-06 23:55:52 +0300

    add sum

--- main.cpp
+++ main.cpp
@@ -6,5 +6,12 @@ using namespace std;
 
 int main()
 {
+    setlocale(0, "");
+    int a, b;
+    cout << "Введите первое целое число" << endl;
+    cin >> a;
+    cout << "Введите второе целое число" << endl;
+    cin >> b;
+    cout << "Сумма чисел = " << a+b;
     return 0;
 }
